32e82de90ad961bea1ec58787127fcf1e7aac259
Manni Wood <manni.lee.wood@gmail.com>
2017-04-21 21:22:30 -0400

    Adds comments on calling convention

--- chap_03/exit.s
+++ chap_03/exit.s
@@ -16,7 +16,31 @@
 .section .text
 .globl _start
 _start:
+
+# 60 is the system call number (linux kernel command number) for exit.
+# It needs to go in rax, because that's where syscall expects to find it.
 movq $60, %rax
-movq $0, %rbx
+
+# 0 is the status number we will return
+movq $0, %rdi
+
+# Spiffy! Assembler for x86 linux has a syscall instruction!
 syscall
+
+# This is an excellent resource on linux x86 syscall conventions:
+# http://stackoverflow.com/questions/2535989/what-are-the-calling-conventions-for-unix-linux-system-calls-on-x86-64
+# But here it is in a nutshell:
+#
+# User-level applications use as integer registers for passing the
+#   sequence %rdi, %rsi, %rdx, %rcx, %r8 and %r9.
+# The kernel interface uses %rdi, %rsi, %rdx, %r10, %r8 and %r9.
+# A system-call is done via the syscall instruction.
+# The kernel destroys registers %rcx and %r11.
+# The number of the syscall has to be passed in register %rax.
+# System-calls are limited to six arguments; no argument is passed directly on the stack.
+# Returning from the syscall, register %rax contains the result of the system-call.
+# A value in the range between -4095 and -1 indicates an error, it is -errno.
+# Only values of class INTEGER or class MEMORY are passed to the kernel.
+
+
 
