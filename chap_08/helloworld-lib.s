43cbfa0ad1c466860f04be49aeb7d6d762cbf807
Manni Wood <manni.lee.wood@gmail.com>
2017-08-25 22:09:44 -0400

    Removes fancy 0 setting

--- chap_08/helloworld-lib.s
+++ chap_08/helloworld-lib.s
@@ -1,11 +1,20 @@
 .section .data
+
 helloworld:
+
 .ascii "hello world\n\0"
+
 .section .text
+
 .globl _start
 _start:
-movq $helloworld, %rdi
-xorq %rax, %rax  # 0 floating point parameters to printf
-call printf
-movq $0, %rdi
-call exit
+  movq $helloworld, %rdi
+  # xorq %rax, %rax  # 0 floating point parameters to printf
+  movq $0, %rax  # 0 floating point parameters to printf
+  call printf
+  movq $0, %rdi
+  call exit
+
+#  C call: RDI, RSI, RDX, RCX, R8, R9
+# Syscall: RDI, RSI, RDX, R10, R8, R9
+
